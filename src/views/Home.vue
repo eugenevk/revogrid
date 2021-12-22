29558791bfcab8b87fb6946369be79856759c625
eugenevk <eugenevk@outlook.com>
2021-12-22 10:06:29 +0100

    Add files via upload

---
+++ src/views/Home.vue
@@ -0,0 +1,31 @@
+<template>
+  <div id="app">
+    <v-grid theme="compact" :source="rows" :columns="columns"/>
+  </div>
+</template>
+
+<script>
+import VGrid from "@revolist/vue3-datagrid";
+export default {
+  name: "App",
+  data() {
+    return {
+      columns: [{
+          prop: "name",
+          name: "First"
+        },
+        {
+          prop: "details",
+          name: "Second"
+      }],
+      rows: [{
+        name: "1",
+        details: "Item 1"
+      }]
+    }
+  },
+  components: {
+    VGrid
+  }
+}
+</script>
